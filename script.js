41bd7ed7d002b18f03cd33b1cd9cafaa5718e275
Ana Paula Negri <85410129+ananegridev@users.noreply.github.com>
2023-06-06 15:16:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const totalResult = document.querySelector('.result div span');
+
+// const reactionTitle = document.querySelector('.react div h4');
+// const reactionIco = document.querySelector('.react div img');
+
+// const memoryTitle = document.querySelector('.memory div h4');
+// const verbalTitle = document.querySelector('.verbal div h4');
+// const visualTitle = document.querySelector('.visual div h4');
+
+function landingPage(){
+    console.log('landingPage function render')
+
+
+}
+
+landingPage();
